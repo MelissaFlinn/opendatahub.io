ace360c11668f921b213ef73f8c8c975b54dc276
lalexandrh <lalexand@redhat.com>
2026-03-16 17:16:55 -0400

    Point to documentation for 3.3 release

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -70,7 +70,7 @@ const config: GatsbyConfig = {
       options: {
         name: `docs`,
         remote: `https://github.com/opendatahub-io/opendatahub-documentation.git`,
-        branch: `v3.0`,
+        branch: `v3.3`,
         local: "public/static/docs",
       },
     },
